da4030717e15067b53da7f964b635d396321c77f
deshket <ketki.deshpande@smail.rasmussen.edu>
2018-02-04 16:37:49 -0600

    Initial commit

---
+++ App4/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App4
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void HplToDoList_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Todolist));
+        }
+
+        private void HplBtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(AuthLogin));
+        }
+
+        //using Amazon.IdentityManagement;
+        //using Amazon.IdentityManagement.Model;
+
+        public static string AWS_Name { get; set; }
+
+        public static void GetAWS()
+        {
+
+            var Var2 = new Amazon.Auth.AccessControlPolicy.Policy();
+            AWS_Name = Convert.ToString(Var2.Version);
+
+        }
+
+
+        private void BtnGetAPI_Click(object sender, RoutedEventArgs e)
+        {
+            GetAWS();
+        }
+
+        private void HplBtnezbooks_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(ezbooks));
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(GoogleAuth));
+        }
+    }
+}
---
+++ dbo.TodoItem.sql
@@ -0,0 +1,31 @@
+﻿CREATE TABLE [dbo].[TodoItem] (
+    [id]        NVARCHAR (255)     CONSTRAINT [DF_TodoItem_id] DEFAULT (CONVERT([nvarchar](255),newid(),(0))) NOT NULL,
+    [createdAt] DATETIMEOFFSET (3) CONSTRAINT [DF_TodoItem_createdAt] DEFAULT (CONVERT([datetimeoffset](3),sysutcdatetime(),(0))) NOT NULL,
+    [updatedAt] DATETIMEOFFSET (3) NULL,
+    [version]   ROWVERSION         NOT NULL,
+    [deleted]   BIT                DEFAULT ((0)) NULL,
+    [BookName]  NVARCHAR (MAX)     NOT NULL,
+    [Author]    NVARCHAR (MAX)     NULL,
+    [ISBN]      NVARCHAR (MAX)     NULL,
+    [Language]  NVARCHAR (MAX)     NULL,
+    PRIMARY KEY NONCLUSTERED ([BookName])
+);
+
+
+GO
+CREATE CLUSTERED INDEX [createdAt]
+    ON [dbo].[TodoItem]([createdAt] ASC);
+
+
+GO
+CREATE TRIGGER [TR_TodoItem_InsertUpdateDelete] ON [dbo].[TodoItem]
+		   AFTER INSERT, UPDATE, DELETE
+		AS
+		BEGIN
+			SET NOCOUNT ON;
+			IF TRIGGER_NESTLEVEL() > 3 RETURN;
+
+			UPDATE [dbo].[TodoItem] SET [dbo].[TodoItem].[updatedAt] = CONVERT (DATETIMEOFFSET(3), SYSUTCDATETIME())
+			FROM INSERTED
+			WHERE INSERTED.id = [dbo].[TodoItem].[id]
+		END
