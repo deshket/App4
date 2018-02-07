5a30b16401293d807b3d22bc5981a090bdd83d0c
deshket <ketki.deshpande@smail.rasmussen.edu>
2018-02-07 13:36:26 -0600

    Add files via upload

---
+++ ezbooks.xaml.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.WindowsAzure.MobileServices;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.Storage;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+using SQLite;
+using SQLite.Net;
+using SQLite.Net.Async;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+using Microsoft.WindowsAzure.MobileServices.SQLiteStore;
+
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App4
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ezbooks : Page
+    {
+        public ezbooks()
+        {
+            this.InitializeComponent();
+        }
+
+        IMobileServiceTable<TodoItem> todoTable = App.MobileService.GetTable<TodoItem>();
+        MobileServiceCollection<TodoItem, TodoItem> items;
+
+
+        public class Todoitem
+        {
+            public string ID { get; set; }
+            public string Text { get; set; }
+            public string BOOKNAME { get; set; }
+            public string AUTHOR { get; set; }
+            public string ISBN { get; set; }
+            public Boolean _deleted { get; set; }
+
+        }
+
+
+        public class TodoItem
+        {
+            public string Id { get; set; }
+            public string Text { get; set; }
+            public bool Complete { get; set; }
+            public string BOOKNAME { get; set; }
+            public string AUTHOR { get; set; }
+            public string ISBN { get; set; }
+        }
+
+        async private void BtnEnter_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                TodoItem item = new TodoItem
+                {
+                    Text = txtboxName.Text,
+                    Complete = false
+                };
+                await App.MobileService.GetTable<TodoItem>().InsertAsync(item);
+                var dialog = new MessageDialog("Successful!");
+                await dialog.ShowAsync();
+            }
+            catch (Exception em)
+            {
+                var dialog = new MessageDialog("An Error Occured: " + em.Message);
+                await dialog.ShowAsync();
+            }
+        }
+
+        public void GetDBSync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new
+                    Uri("https://kdserver.database.windows.net");
+
+                var json = client.GetStringAsync("/").Result;
+                var contacts = JsonConvert.DeserializeObject<TodoItem[]>(json);
+            }
+        }
+
+        private async Task RefreshTodoItems()
+        {
+            MobileServiceInvalidOperationException exception = null;
+            try
+            {
+                // This code refreshes the entries in the list view by querying the TodoItems table.
+                // The query excludes completed TodoItems
+                items = await todoTable
+                    .Where(TodoItem => TodoItem.Complete == false)
+                    .ToCollectionAsync();
+            }
+            catch (MobileServiceInvalidOperationException e)
+            {
+                exception = e;
+            }
+
+            if (exception != null)
+            {
+                await new MessageDialog(exception.Message, "Error loading items").ShowAsync();
+            }
+            //else
+            //{
+            //    ListItems.ItemsSource = items;
+            //    this.BtnRefresh.IsEnabled = true;
+            //}
+        }
+
+        private async void CheckBoxComplete_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = (CheckBox)sender;
+            TodoItem item = cb.DataContext as TodoItem;
+            await UpdateCheckedTodoItem(item);
+        }
+
+
+        //private async Task UpdateCheckedTodoItem(TodoItem item)
+        //{
+        //    // This code takes a freshly completed TodoItem and updates the database. When the MobileService 
+        //    // responds, the item is removed from the list 
+        //    await todoTable.UpdateAsync(item);
+        //    items.Remove(item);
+        //    ListItems.Focus(Windows.UI.Xaml.FocusState.Unfocused);
+
+
+            private Task UpdateCheckedTodoItem(TodoItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+            async private void BtnRefresh_Click(object sender, RoutedEventArgs e)
+            {
+                await RefreshTodoItems();
+            }
+
+            private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+
+
+        
+
+       
+    }
+}
